0f185d5678e233491845e39d7a4f2fc7e9353492
luisgomez29 <luisgaleano002@gmail.com>
2023-06-17 18:56:35 -0500

    test: Add test for api endpoint

--- ms_cinema/infrastructure/entry-points/reactive-web/src/test/java/co/com/luisgomez29/api/RouterRestTest.java
+++ ms_cinema/infrastructure/entry-points/reactive-web/src/test/java/co/com/luisgomez29/api/RouterRestTest.java
@@ -1,60 +1,46 @@
 package co.com.luisgomez29.api;
 
+import co.com.luisgomez29.api.config.WebFluxSecurityConfig;
+import co.com.luisgomez29.model.genre.Genre;
+import co.com.luisgomez29.usecase.cinema.CinemaUseCase;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Flux;
 
-@ContextConfiguration(classes = {RouterRest.class, Handler.class})
+import static org.mockito.Mockito.when;
+
+@ContextConfiguration(classes = {
+        WebFluxSecurityConfig.class,
+        RouterRest.class,
+        Handler.class,
+})
 @WebFluxTest
 class RouterRestTest {
 
     @Autowired
     private WebTestClient webTestClient;
 
+    @MockBean
+    private CinemaUseCase useCase;
+
     @Test
     void testListenGETUseCase() {
-        webTestClient.get()
-                .uri("/api/usecase/path")
-                .accept(MediaType.APPLICATION_JSON)
-                .exchange()
-                .expectStatus().isOk()
-                .expectBody(String.class)
-                .value(userResponse -> {
-                            Assertions.assertThat(userResponse).isEmpty();
-                        }
-                );
-    }
+        when(useCase.getAll())
+                .thenReturn(Flux.just(Genre.builder().id(1).name("Comedia").build()));
 
-    @Test
-    void testListenGETOtherUseCase() {
         webTestClient.get()
-                .uri("/api/otherusercase/path")
+                .uri("/api/v1/genres")
                 .accept(MediaType.APPLICATION_JSON)
                 .exchange()
                 .expectStatus().isOk()
                 .expectBody(String.class)
-                .value(userResponse -> {
-                            Assertions.assertThat(userResponse).isEmpty();
-                        }
-                );
-    }
-
-    @Test
-    void testListenPOSTUseCase() {
-        webTestClient.post()
-                .uri("/api/usecase/otherpath")
-                .accept(MediaType.APPLICATION_JSON)
-                .bodyValue("")
-                .exchange()
-                .expectStatus().isOk()
-                .expectBody(String.class)
-                .value(userResponse -> {
-                            Assertions.assertThat(userResponse).isEmpty();
-                        }
+                .value(userResponse -> Assertions.assertThat(userResponse).isNotEmpty()
                 );
     }
 }
