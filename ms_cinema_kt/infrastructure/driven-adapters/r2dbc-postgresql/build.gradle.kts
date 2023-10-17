8659089dea6de4f31ae7131d9b5a9e1bac7db2a5
luisgomez29 <luisgaleano002@gmail.com>
2023-10-16 20:58:26 -0500

    build(ms_cinema_kt): Remove code comment in r2dbc gradle file

--- ms_cinema_kt/infrastructure/driven-adapters/r2dbc-postgresql/build.gradle.kts
+++ ms_cinema_kt/infrastructure/driven-adapters/r2dbc-postgresql/build.gradle.kts
@@ -9,14 +9,3 @@ dependencies {
     implementation("org.mapstruct:mapstruct:1.5.5.Final")
     kapt("org.mapstruct:mapstruct-processor:1.5.5.Final")
 }
-
-//dependencies {
-//    implementation(kotlin("stdlib-jdk8"))
-//    implementation("org.mapstruct:mapstruct:1.5.5.Final")
-//    kapt("org.mapstruct:mapstruct-processor:1.5.5.Final")
-//
-//    testImplementation("org.junit.jupiter:junit-jupiter-api:5.3.1")
-//    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.3.1")
-//    testImplementation("org.assertj:assertj-core:3.11.1")
-//}
-
